64e8d3eb5b1cba2c2d39c95e8c0c66f5301938b4
Mamun <abdullahalmamun914@gmail.com>
2019-05-12 15:16:38 +0600

    update file

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,14 @@
 
 const weatherForm=document.querySelector('form')
-const search=document.querySelector('input').value
+const search=document.querySelector('input')
     
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     document.querySelector('.message-1').textContent='Loading...'
     document.querySelector('.message-2').textContent=' '
+    const location=search.value
 
-    fetch(`/weather?address=${search}`).then((response)=>{
+    fetch('/weather?address=' +location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             document.querySelector('.message-1').textContent=data.error
